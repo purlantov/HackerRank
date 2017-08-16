4a10057aa6e5790d435a4e9c242832a32ff6c071
purlantov <ppurlantov@yahoo.com>
2017-08-16 17:12:13 +0300

    initial commit

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+public class MainClass
+{
+	public static void Main(String[] args)
+	{
+		var array = new int[6, 6];
+
+		for (int i = 0; i < 6; i++)
+		{
+			var input = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+			for (int j = 0; j < 6; j++)
+			{
+				array[i, j] = input[j];
+			}
+		}
+
+		int sum = 0;
+		int max = int.MinValue;
+
+
+		for (int row = 0; row < 4; row++)
+		{
+			for (int col = 0; col < 4; col++)
+			{
+				sum += array[row, col] + array[row, col + 1] + array[row, col + 2];
+				sum += array[row + 1, col + 1];
+				sum += array[row + 2, col] + array[row + 2, col + 1] + array[row + 2, col + 2];
+				if (sum > max)
+				{
+					max = sum;
+				}
+				sum = 0;
+			}
+
+		}
+
+		Console.WriteLine(max);
+	}
+}
---
+++ Arrays/DynamicArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DynamicArray
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
