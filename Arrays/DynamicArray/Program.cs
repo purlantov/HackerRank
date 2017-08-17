a0eb325cee524543174860df303863dc1ad93e90
purlantov <ppurlantov@yahoo.com>
2017-08-17 14:04:17 +0300

    Dynamic Array

--- Arrays/DynamicArray/Program.cs
+++ Arrays/DynamicArray/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DynamicArray
 {
-	class MainClass
+	public class MainClass
 	{
-		public static void Main(string[] args)
+		public static void Main()
 		{
-			Console.WriteLine("Hello World!");
+			var firstInput = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+			int n = firstInput[0];
+			int q = firstInput[1];
+			int lastAnswer = 0;
+			var array = new int[q][];
+			//int result = 0;
+
+
+			for (int i = 0; i < q; i++)
+			{
+				var input = Console.ReadLine().Split(' ').Select(v => Convert.ToInt32(v)).ToArray();
+				int t = input[0];
+				int x = input[1];
+				int y = input[2];
+
+				if (t == 1)
+				{
+					array[(x ^ lastAnswer) % n] = new int[]{y};
+				}
+				else
+				{
+					lastAnswer = array[(x ^ lastAnswer) % n][y % array[(x ^ lastAnswer) % n].Count()];
+					Console.WriteLine(lastAnswer);
+				}
+			}
 		}
 	}
 }
+
+//int n, q;
+//cin >> n >> q;
+//
+//vector<vector<int>> s(n, vector<int> ());
+//int lastAnswer = 0;
+//
+//for (int i = 0; i<q;i++) 
+//{
+//    int t, x, y;
+//	cin >> t >> x >> y;
+//
+//    if (t==1) {
+//        s[(x ^ lastAnswer) % n].push_back(y);
+//    }
+//    else {
+//        lastAnswer=s[(x ^ lastAnswer) % n][y % s[(x ^ lastAnswer) % n].size()];
+//        cout << lastAnswer << endl;
+//    }
+//}
