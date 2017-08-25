894bed2047da1b52131850d2f38c1b6f2b946f0a
purlantov <ppurlantov@yahoo.com>
2017-08-25 12:48:26 +0300

    Practice Arrays

--- Arrays/DynamicArray/Program.cs
+++ Arrays/DynamicArray/Program.cs
@@ -8,24 +8,24 @@ namespace DynamicArray
 	{
 		public static void Main()
 		{
-			var firstInput = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+			var firstInput = Console.ReadLine().Trim().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
 			int n = firstInput[0];
 			int q = firstInput[1];
 			int lastAnswer = 0;
-			var array = new int[q][];
-			//int result = 0;
+			var array = Enumerable.Range(0, n).Select((i) => new List<int>()).ToArray();
+			//int index= 0;
 
 
 			for (int i = 0; i < q; i++)
 			{
-				var input = Console.ReadLine().Split(' ').Select(v => Convert.ToInt32(v)).ToArray();
+				var input = Console.ReadLine().Trim().Split(' ').Select(v => Convert.ToInt32(v)).ToArray();
 				int t = input[0];
 				int x = input[1];
 				int y = input[2];
 
 				if (t == 1)
 				{
-					array[(x ^ lastAnswer) % n] = new int[]{y};
+					array[(x ^ lastAnswer) % n].Add(y);
 				}
 				else
 				{
---
+++ Arrays/LeftRotation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Solution
+{
+	public static void Main(String[] args)
+	{
+		var tokens_n = Console.ReadLine().Split(' ');
+		int n = Convert.ToInt32(tokens_n[0]);
+		int d = Convert.ToInt32(tokens_n[1]);
+		var a_temp = Console.ReadLine().Split(' ');
+		var array = Array.ConvertAll(a_temp, Int32.Parse);
+		int[] result = leftRotation(array, d);
+		Console.WriteLine(String.Join(" ", result));
+
+
+	}
+
+	public static int[] leftRotation(int[] array, int d)
+	{
+		// Complete this function
+		Reverse(array, 0, d - 1);
+		Reverse(array, d, array.Length-1);
+		Reverse(array, 0, array.Length-1);
+
+		return array;
+	}
+
+	public static void Swap(int[] array, int a, int b)
+	{
+		int tmp = array[a];
+		array[a] = array[b];
+		array[b] = tmp;
+	}
+
+	public static void Reverse(int[] array, int start, int end)
+	{
+		for (int i = start; i <= (start + end) / 2; i++)
+		{
+			Swap(array, i, (start + end) - i);
+		}
+	}
+}
